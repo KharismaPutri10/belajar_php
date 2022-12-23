bb98c3db22706ffb705de2a99e31861cbfdeb2d2
KharismaPutri <113002140+KharismaPutri10@users.noreply.github.com>
2022-12-23 22:21:18 +0700

    Update php dasar kedua.php

--- belajar-php-php_mysql/php dasar kedua.php
+++ belajar-php-php_mysql/php dasar kedua.php
@@ -1,9 +1,9 @@
 <?php
 
-$NamaLengkap = "Christian Titus Fernanda" ;
-$NamaPanggilan = "Titus" ;
+$NamaLengkap = "Kharisma Putri Naisya Umami" ;
+$NamaPanggilan = "Putri atau Kharisma" ;
 
-    echo "Halo, perkenalkan nama saya $NamaLengkap, biasa dipanggil $NamaPanggilan." ;
+    echo "Halo, perkenalkan nama saya $NamaLengkap, biasa dipanggil $NamaPanggilan. Saya berasal dari desa Borobudur, Magelang, Jawa Tengah." ;
 
 
 ?>
